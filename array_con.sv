c2f2902477995f42b4b5e497fbab620c50790baa
mani <manikandanadmv@gmail.com>
2026-01-30 17:34:04 +0530

    commit-7

--- array_con.sv
+++ array_con.sv
@@ -13,7 +13,28 @@ class A1;
         data1[i] inside {[15:30]};
 
     }
+///////////   task-1
+    rand bit [7:0] data_2[4];
 
+    constraint c3 {
+        data_2[0] inside {[8'h10:8'hff]};
+
+        foreach(data_2[i]){
+        if(data[1]>0)
+        data_2[i] inside {[8'h20:8'h2f]};
+        }
+    }
+////////// task-2
+    rand int data_3[5];
+
+    constraint c4 {
+        
+        foreach(data_3[i]){
+        data_3[i] inside {[0:50]};
+        if(i>0)
+        data_3[i] >= data_3[i-1];
+        }
+    }
 endclass
 
 module tb;
@@ -23,7 +44,7 @@ module tb;
         a1=new();
         repeat(10) begin
             a1.randomize();
-            $display("data=%0p ||  data1=%0p",a1.data,a1.data1);
+            $display("data=%0p ||  data1=%0p   || data_2=%0p || data_3=%0p ",a1.data,a1.data1,a1.data_2,a1.data_3);
         end
     end
 endmodule
